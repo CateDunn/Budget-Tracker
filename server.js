edc4c7c039625cef9783e94375fee6faaff07251
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-29 11:39:04 -0400

    updating connection

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true
-});
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
 
 // routes
 app.use(require("./routes/api.js"));
